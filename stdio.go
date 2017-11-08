b6c68ff917cb4c48afdc8c7a788a23e06c5ad9e7
Tim Peoples <tim@toolman.org>
2017-11-08 14:49:41 -0800

    Update import constraint (again)

--- stdio.go
+++ stdio.go
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-package stdio
+package stdio // import "toolman.org/io/stdio"
 
 import (
 	"fmt"
